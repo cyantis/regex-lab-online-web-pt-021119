6f865bda7771fd0062ff9af7ce9ba194d1f64490
Cody Yantis <cyantis@gmail.com>
2019-02-24 02:08:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  binding.pry
+  word.match(/^\w[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
 end
 
 def words_five_letters_long(text)
