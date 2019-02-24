8f1fa02c817d05f789974cebd1cdb93de76d29d4
Cody Yantis <cyantis@gmail.com>
2019-02-24 02:28:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  word.scan(/^[aeiou]/).empty? ? false : true
+  x = word.scan(/^[aeiou]/)
+  !x.empty?
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
